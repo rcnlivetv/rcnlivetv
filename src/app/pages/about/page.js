d82a242b3bedcae95c3b2107b15e5a7cc90d9681
rcnlivetv <rcnlivetv@gmail.com>
2024-10-04 02:25:19 +0600

    Update page.js

--- src/app/pages/about/page.js
+++ src/app/pages/about/page.js
@@ -13,10 +13,10 @@ const About=()=>{
             <div className="about-info">
             <p className="abt-header">KNOW US </p>
            <ol style={{fontSize:16}}>
-           <li>We serve free and Paid live TV streaming</li> 
+           <li>We serve free and paid live TV streaming</li> 
            <li>Affordable and premium service</li> 
-           <li>Our appliction is easy</li> 
-           <li>Connect to Android TV and phone and enjoy </li> 
+           <li>Our application is easy to use</li> 
+           <li>Connect to Android TV and phone and enjoy :)</li> 
 
            </ol>
             </div>
