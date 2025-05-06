7d8b674f4c217e2473a317abc4050f34e428e285
rcnlivetv <rcnlivetv@gmail.com>
2025-05-06 23:11:49 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -45,6 +45,37 @@ async headers() {
     },
   ]
 },
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`
+ 
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ]
+  },
+}
+
+
+
 }
 
 module.exports = nextConfig
