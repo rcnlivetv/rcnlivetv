fba8f07550f116cfea272b11f5878b09463666a6
rcnlivetv <rcnlivetv@gmail.com>
2025-05-06 23:00:25 +0600

    Update channelList.js
    ok hit changes

--- src/app/tvjson/channelList.js
+++ src/app/tvjson/channelList.js
@@ -2,7 +2,7 @@ const channelsDatas=[
       {
         id:1,
         title:'live',
-        category:'RCN live',logoUrl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBAY79s7ORXoJSLNAs2Yp0wMA9tn_bHCfG1Q&s',
+        category:'RCN live',logoUrl:'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/T_Sports_logo.svg/1200px-T_Sports_logo.svg.png',
         liveUrl:'http://38.96.178.201/live/TSports/index.m3u8'
       },
       {
