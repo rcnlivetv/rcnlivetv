8d4e3c3251a633d939b15955366d4beb864bd2e6
rcnlivetv <rcnlivetv@gmail.com>
2025-05-06 23:44:15 +0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
-'use client'
+//'use client'
 import {useEffect,useState } from "react";
 import { subscribeUser, unsubscribeUser, sendNotification } from './actions'
 import AppPage from './MainPage'
-//import { headers } from 'next/headers'
+import { headers } from 'next/headers'
  
 //export default async function Page() {
 
@@ -136,9 +136,9 @@ function InstallPrompt() {
   );
 }
  
-export default function Home() {
-//  const headersList = await headers()
-//  const userAgent = headersList.get('user-agent')
+export default async function Home() {
+  const headersList = await headers()
+  const userAgent = headersList.get('user-agent')
   return (
     <div>
       <AppPage/>
